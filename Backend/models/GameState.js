2b88e99c9f2089807f933a6059e6acaeb62e07ec
siddhant4357 <156295454+siddhant4357@users.noreply.github.com>
2025-03-29 13:43:01 +0530

    Update GameState.js

--- Backend/models/GameState.js
+++ Backend/models/GameState.js
@@ -42,7 +42,29 @@ const gameStateSchema = new mongoose.Schema({
       type: Date,
       default: Date.now
     }
-  }]
+  }],
+  activeUsers: [{
+    username: String,
+    socketId: String,
+    lastSeen: Date
+  }],
+  reconnectionToken: String
 }, { timestamps: true });
+
+gameStateSchema.methods.handleReconnection = async function(username, socketId) {
+  const user = this.activeUsers.find(u => u.username === username);
+  if (user) {
+    user.socketId = socketId;
+    user.lastSeen = new Date();
+  } else {
+    this.activeUsers.push({
+      username,
+      socketId,
+      lastSeen: new Date()
+    });
+  }
+  await this.save();
+  return this;
+};
 
 module.exports = mongoose.model('GameState', gameStateSchema);

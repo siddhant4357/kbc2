00e7af17cb1a4af37b4eefe412291b23e9d9e7eb
siddhant4357 <156295454+siddhant4357@users.noreply.github.com>
2025-03-29 15:27:58 +0530

    Update config.js

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const config = {
   PORT: process.env.PORT || 4000,
   MONGODB_URI: process.env.MONGODB_URI,
-  CLIENT_URL: process.env.NODE_ENV === 'production' 
-    ? process.env.CLIENT_URL 
-    : 'http://localhost:5173',
+  CLIENT_URLS: process.env.NODE_ENV === 'production'
+    ? (process.env.CLIENT_URL || '').split(',')
+    : ['http://localhost:5173'],
   JWT_SECRET: process.env.JWT_SECRET,
   uploadPath: 'uploads'
 };

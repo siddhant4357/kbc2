de28ccd6deb07f79373e55bea53b051ed6aa5b1b
siddhant4357 <156295454+siddhant4357@users.noreply.github.com>
2025-04-05 12:58:45 +0530

    Update config.js

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -6,9 +6,7 @@ const config = {
   CLIENT_URLS: process.env.NODE_ENV === 'production'
     ? [
         'https://kbc-frontend-beige.vercel.app',
-        'https://kbc-frontend-1-git-main-siddhants-projects-bf927e7a.vercel.app',
-        'https://*.firebaseapp.com',
-        'https://*.firebasedatabase.app'
+        'https://kbc-frontend-1-git-main-siddhants-projects-bf927e7a.vercel.app'
       ]
     : ['http://localhost:5173'],
   JWT_SECRET: process.env.JWT_SECRET,

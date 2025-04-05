965231a41269e5141fb9b3ea3334c143f00f1c04
siddhant4357 <156295454+siddhant4357@users.noreply.github.com>
2025-04-05 10:34:11 +0530

    Update config.js

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -4,7 +4,12 @@ const config = {
   PORT: process.env.PORT || 4000,
   MONGODB_URI: process.env.MONGODB_URI,
   CLIENT_URLS: process.env.NODE_ENV === 'production'
-    ? (process.env.CLIENT_URL || '').split(',')
+    ? [
+        'https://kbc-frontend-beige.vercel.app',
+        'https://kbc-frontend-1-git-main-siddhants-projects-bf927e7a.vercel.app',
+        'https://*.firebaseapp.com',
+        'https://*.firebasedatabase.app'
+      ]
     : ['http://localhost:5173'],
   JWT_SECRET: process.env.JWT_SECRET,
   uploadPath: 'uploads'

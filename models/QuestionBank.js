a89f85dc9854ffa1da7576b8705dd17e511a1c62
siddhant4357 <156295454+siddhant4357@users.noreply.github.com>
2025-04-14 15:05:34 +0530

    Add files via upload

---
+++ models/QuestionBank.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const questionBankSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  passcode: {
+    type: String,
+    required: true,
+    length: 4
+  },
+  questions: [{
+    question: String,
+    options: [String],
+    correctAnswer: String,
+    imageUrl: String // Add this field
+  }],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('QuestionBank', questionBankSchema);

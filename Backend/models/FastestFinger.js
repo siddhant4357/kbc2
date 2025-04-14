d3b189e5ddeed4993406e949802044010b732dc4
siddhant4357 <156295454+siddhant4357@users.noreply.github.com>
2025-04-14 14:09:29 +0530

    Create FastestFinger.js

---
+++ Backend/models/FastestFinger.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const fastestFingerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  passcode: {
+    type: String,
+    required: true,
+    length: 4
+  },
+  question: {
+    text: String,
+    options: [String],
+    correctSequence: [Number], // Stores the correct order as array of indices
+    imageUrl: String
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('FastestFinger', fastestFingerSchema);
